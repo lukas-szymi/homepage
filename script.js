6d8f733eaecee61dcb556f3eb05f5caa59eb2480
Lukasz Szymanski <szymanski1993@gmail.com>
2020-01-15 22:09:30 +0100

    added simply test for querySelector in js file

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const testValue = 7;
+
+var emptyContainer = document.querySelector('.empty__container--js');
+
+console.log(emptyContainer);
+
+emptyContainer.innerHTML = `Some text ${testValue}`;

77bd4c0130cbed98f4ae0825a4d3a531f2be51ba
Lukasz Szymanski <szymanski1993@gmail.com>
2020-01-21 21:25:13 +0100

    Added prompt windows for first name and last name

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ var emptyContainer = document.querySelector('.empty__container--js');
 console.log(emptyContainer);
 
 emptyContainer.innerHTML = `Some text ${testValue}`;
+
+let firstName = prompt("What is your name");
+let lastName = prompt("What is you las name");
+
+getUserName(firstName,lastName);
+
+function getUserName(firstName, lastName){
+  console.log(`My name is ${firstName} and my lastname is ${lastName}`);
+}
